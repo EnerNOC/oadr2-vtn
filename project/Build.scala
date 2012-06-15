f2288264f2adc1b997638717676b7fa1b861db2c
Jeffrey LaJoie <jlajoie@enernoc.com>
2012-06-15 16:56:53 -0400

    Missing build.scala file

--- project/Build.scala
+++ project/Build.scala
@@ -4,15 +4,17 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "testApp"
+    val appName         = "play-oadr2-model"
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      // Add your project dependencies here,
+    	//not certain of the .org is necessary
+   		"org.hibernate" % "hibernate-entitymanager" % "4.1.4.Final"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
-      // Add your own project settings here      
+   	  //added to include the oadr2.model library to be referenceable(sp?) in the views package
+      templatesImport += "org.enernoc.open.oadr2.model._"    
     )
 
 }
