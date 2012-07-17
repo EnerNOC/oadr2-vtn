72dab6af568749d761e7d7e2fa49d7512ec3b9bb
Jeffrey LaJoie <jlajoie@enernoc.com>
2012-07-17 11:29:44 -0400

    Added more appropriate handling of the XML request handling

--- app/controllers/Requests.java
+++ app/controllers/Requests.java
@@ -58,30 +58,17 @@ public class Requests extends Controller{
 			return badRequest("XML is invalid.\n");
 		}
 		
-		/*
-		//shouldn't have to hard code these, should check to see if can parse from XPath
-		//also shouldnt name it happyMappy but it made me smile =D
-		Map<String, String> happyMappy = new HashMap<String, String>();
-		happyMappy.put("pyld", "http://docs.oasis-open.org/ns/energyinterop/201110/payloads");
-		happyMappy.put("oadr", "http://openadr.org/oadr-2.0a/2012/03");
-		happyMappy.put("emix", "http://docs.oasis-open.org/ns/emix/2011/06");
-		happyMappy.put("ei", "http://docs.oasis-open.org/ns/energyinterop/201110");
-		happyMappy.put("xsi", "http://www.w3.org/2001/XMLSchema-instance");
-		//Logger.info(XPath.selectText("//emix:marketContext", document, happyMappy));
-		*/
-		
 		JAXBContext jaxbContext = JAXBContext.newInstance("org.enernoc.open.oadr2.model");
 		Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
 		Marshaller marshaller = jaxbContext.createMarshaller();
 		Object o = unmarshaller.unmarshal(document);
 		
-		//should make all instance ofs own methods probably, also still not do instanceof cause bad
-		
+		//should make all instance ofs own methods probably, also still not do instanceof cause bad		
 		//if RequestEvent send out a response!
-		if(o instanceof OadrRequestEvent){
+		if(o instanceof OadrRequestEvent){						
 			OadrRequestEvent oRequestEvent = (OadrRequestEvent) o;
-			EiResponse e = new EiResponse();
-			e.setRequestID(oRequestEvent.getEiRequestEvent().getRequestID());
+			Logger.info(oRequestEvent.toString());
+			EiResponse eiResponse = new EiResponse();
 			/*
 			 * 1xx Informational - Request received, continuing process
 			 * 2xx Success - The request was successfully received, understood, and accepted
@@ -89,47 +76,43 @@ public class Requests extends Controller{
 			 * 4xx Requester Error - The request contains bad syntax or cannot be fulfilled
 			 * 5xx Responder Error - The responder failed to fulfill an apparently valid request
 			 */
-			//be sure to do error handling if it is not a 1 or 2 first char type thing
-			e.setResponseCode("000");//shouldnt be 000, check out 5.6 of the EiResponse page
-			e.setResponseDescription("Optional! But I'm here <3");
-			Object response = new OadrResponse().withEiResponse(e);
+
+			eiResponse.setRequestID(oRequestEvent.getEiRequestEvent().getRequestID());
+			eiResponse.setResponseCode("200");
+			eiResponse.setResponseDescription("Optional! But I'm here <3");
+						
+			OadrDistributeEvent response = new OadrDistributeEvent().withEiResponse(eiResponse);
+			//Need to find out how to get EiEvent(s) and add them to this Distribute Event
+			//Possibly from the Market Context, but need XPath to access, or find through VEN and Users
+			//response.withOadrEvent(null);
+			response.withRequestID(eiResponse.getRequestID());
 			
 			StringWriter sw = new StringWriter();
 			marshaller.marshal(response, sw);
 			response().setContentType("application/xml");
+			//probably should not return a string and find a way to format xml
 			return ok(sw.toString() + '\n');
 		}
 		//if its a created event send out a distribute event
 		else if(o instanceof OadrCreatedEvent){
 			OadrCreatedEvent oCreatedEvent = (OadrCreatedEvent) o;
-			//check the oCreatedEvent.eiresponse for an OKAY message
-			//then check the other eventresponses for okay responses
-			OadrDistributeEvent distributeEvent = new OadrDistributeEvent();
-			EiResponse eiResponse = new EiResponse();
 			
-			eiResponse.withRequestID(oCreatedEvent.getEiCreatedEvent().getEiResponse().getRequestID());		
-			
-			//eiResponse.withResponseCode(null);
-			//eiResponse.withResponseDescription(null);			
-			//response.withEiResponse(null);
-			//response.withOadrEvent(null);
-			
-			//not sure if should set the vtn to the id the created event comes from
-			//response.withVtnID(oCreatedEvent.getEiCreatedEvent().getVenID());
-			distributeEvent.withEiResponse(eiResponse);
-			distributeEvent.withRequestID(oCreatedEvent.getEiCreatedEvent().getEiResponse().getRequestID());
+			//Be sure to set the response code correctly to 1-5 depending on how the event is handled, temp for now
+			EiResponse eiResponse = new EiResponse()
+				.withRequestID(oCreatedEvent.getEiCreatedEvent().getEiResponse().getRequestID())
+				.withResponseCode("200")
+				.withResponseDescription("Optional description!");						
 			
 			StringWriter sw = new StringWriter();
-			marshaller.marshal(distributeEvent, sw);
+			marshaller.marshal(new OadrResponse(eiResponse), sw);
 			response().setContentType("application/xml");
 			return ok(sw.toString() + '\n');
 		}
-		//if its a distribute event...
-		/*
-		else if(o instanceof OadrDistributeEvent){
-			OadrDistributeEvent oDistributeEvent = (OadrDistributeEvent) o;
+		//occurs after sending out a DistributeEvent
+		else if(o instanceof OadrResponse){
+			OadrResponse oResponse = (OadrResponse) o;
+			//this is where the Comet/Websocket stuff will happen to populate the table!
 		}
-		*/
 		
 		return ok("Hallo! Things didn't error out, but should not have reached here!\n");
 	} 
